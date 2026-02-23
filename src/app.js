6349bee2c3043f7b3a1b869b68979f1c98811630
shruthikanimeta <shruthikanimeta031@gmail.com>
2026-02-23 23:51:36 +0000

    get APIS

--- src/app.js
+++ src/app.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const { connectDB } = require("./config/database");
-const User = require('./models/user')
+const User = require("./models/user");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("something went wrong" + err);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong" + err);
+  }
+});
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
   await user.save();
-  try{
-res.send("user added successfully");
+  try {
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(404).send("error found");
   }
-  catch(err) {
-    res.status(404).send('error found')
-  }
-  
 });
 
 connectDB()
